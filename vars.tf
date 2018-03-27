40fb744f76e572b939be9daa80f707f983b52d24
Adrian Navarro <adrian.navarro@greenlightpower.net>
2018-03-27 17:01:43 +0100

    Added Instance

--- vars.tf
+++ vars.tf
@@ -9,3 +9,13 @@ variable "PATH_TO_PRIVATE_KEY" {
 variable "PATH_TO_PUBLIC_KEY" {
   default = "adrian.pub"
 }
+
+variable "AMIS" {
+  type = "map"
+
+  default = {
+    us-east-1 = "ami-13be557e"
+    us-west-2 = "ami-06b94666"
+    eu-west-1 = "ami-844e0bf7"
+  }
+}
